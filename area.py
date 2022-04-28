ae41f9878f542ccfad6306cd47f4180c4cc18681
lanzeloth03 <lvargasmarichi@gmail.com>
2022-04-28 11:52:26 -0500

    mi primera vez

---
+++ area.py
@@ -0,0 +1,9 @@
+Algoritmo sin_titulo
+	definir b, a, area como Real 
+	escribir "ingrese la base"
+	leer b
+	escribir "ingrese altura"
+	leer a
+	area<-b*a
+	escribir "Area de R es:" area 
+FinAlgoritmo
