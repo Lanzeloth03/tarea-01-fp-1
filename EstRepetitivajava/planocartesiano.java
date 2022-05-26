0beb263b780564d0393a9f810052f2c577d0cc47
Lanzeloth03 <103522140+Lanzeloth03@users.noreply.github.com>
2022-05-26 11:59:21 -0500

    lanzeloth

--- EstRepetitivajava/planocartesiano.java
+++ EstRepetitivajava/planocartesiano.java
@@ -31,5 +31,31 @@ public class GraficarLetras {
     public static void main(String[] args) {
         graficarA();
     }
-
+    
+    public static void graficararboldenavidad() {
+        System.out.println("Ingese Altura de la Letra:"); 
+        int cantF=leerT.nextInt();
+        System.out.println("Ingrese num fila para Linea:");
+        int numFilLinea=leerT.nextInt();
+        int medio=cantF+1;
+        System.out.println("ingrese numero de filas del cuadrado ");
+        int d = leerT.nextInt();
+         for (int f = 1; f <= cantF; f++) {
+             for (int c = 1; c <= 2*cantF+1; c++) {
+                 if (f==1 && medio==c) {
+                     System.out.print("*");
+                 }else if(c==medio-f && f>1){
+                     System.out.print("*");
+                 }else if(c==medio+f && f>1){
+                     System.out.print("*");
+                 }else if(f==numFilLinea && c>=medio-f && c<=medio+f){
+                     System.out.print("*");
+                 }else{
+                     System.out.print(" ");
+                 }
+             }
+             System.out.print("\n");
+         }
+         System.out.println("");
+     }
 }
