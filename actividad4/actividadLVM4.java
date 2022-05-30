354607ca2ac4116bfcc79021d76806c60519610e
Lanzeloth03 <103522140+Lanzeloth03@users.noreply.github.com>
2022-05-29 20:04:11 -0500

    lanzeloth

--- actividad4/actividadLVM4.java
+++ actividad4/actividadLVM4.java
@@ -138,9 +138,9 @@ public class actividad4 {
         //Definir variables
         int nm;
         int venta;
-        double yho=0;
-        double ja=0;
-        double na=0;
+        double a=0;
+        double b=0;
+        double c=0;
         double t=0,t1=0,t2=0,t3=0;
         //datos de entrada
         System.out.println("Ingrese el numero de ventas realizadas: ");
@@ -149,23 +149,23 @@ public class actividad4 {
             System.out.println("Ingresa el monto de la venta realizada "+nm);
             venta=lt.nextInt();
             if (venta>=20000){
-			    yho = yho + 1;
+			    a = a + 1;
 			    t1 = t1+venta;
             }else if (venta>=10000 && venta<20000){
-				ja = ja + 1;
+				b = b + 1;
 				t2 = t2 + venta;
             }else{
-				na = na + 1;
+				c = c + 1;
 				t3 = t3 + venta;
             }
 			t = t + venta;
 		    nm = nm - 1;
        }
-        System.out.println("Numero de ventas mayores a $20,000 es: "+yho);
+        System.out.println("Numero de ventas mayores a $20,000 es: "+a);
         System.out.println("El total de las ventas mayores a $20,000 es: "+t1);
-	    System.out.println("Numero de ventas mayores a $10,000 y menores a $20,000 es: "+ja);
+	    System.out.println("Numero de ventas mayores a $10,000 y menores a $20,000 es: "+b);
 	    System.out.println("El total de las ventas mayores a $10,000 y menores a $20,000 es: "+t2);
-	    System.out.println("Numero de ventas menores a $10,000 es: "+na);
+	    System.out.println("Numero de ventas menores a $10,000 es: "+c);
 	    System.out.println("El total de las ventas menores a $10,000: "+t3);
 	    System.out.println("El monto total de la ventas es: $"+t);
     }
@@ -258,6 +258,6 @@ public class actividad4 {
         System.out.println((suma - numero) / (n - 1));
       }
     public static void main(String[] args) {
-        actividadLVM424();
+        actividadLVM412();
     }
 }
